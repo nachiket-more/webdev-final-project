b7ef460816b491148ad51dcdea138e5dfea6994b
Nachiket <nmore@pdx.edu>
2023-03-14 23:16:17 -0700

    click event listner for sidebar

---
+++ index.js
@@ -0,0 +1,11 @@
+const openMenu = document.getElementById("open-icon");
+openMenu.addEventListener("click", function(){
+    document.querySelector(".small-menu").style.display = "flex"
+    document.querySelector(".small-menu").style.right = "0"
+});
+
+const closeMenu = document.getElementById("close-icon");
+closeMenu.addEventListener("click", function(){
+    document.querySelector(".small-menu").style.right = "-100%"
+});
+
