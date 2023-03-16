6b62d003a4a46a024cace6c045f150f2ba91b96c
Nachiket <nmore@pdx.edu>
2023-03-15 17:19:29 -0700

    made changes to fix accessibility and validation errors

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const openMenu = document.getElementById("open-icon");
-openMenu.addEventListener("click", function(){
-    document.querySelector(".small-menu").style.display = "flex"
-    document.querySelector(".small-menu").style.right = "0"
+openMenu.addEventListener("click", function () {
+  document.querySelector(".small-menu").style.display = "flex";
+  document.querySelector(".small-menu").style.right = "0";
 });
 
 const closeMenu = document.getElementById("close-icon");
-closeMenu.addEventListener("click", function(){
-    document.querySelector(".small-menu").style.right = "-100%"
+closeMenu.addEventListener("click", function () {
+  document.querySelector(".small-menu").style.right = "-100%";
 });
-
